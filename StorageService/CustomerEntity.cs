ef4e58137cc0746ff64dc354d35854cef358ee2c
Jakub Gabała <jakub.gabala@gmail.com>
2019-05-18 22:13:17 +0200

    progress

---
+++ StorageService/CustomerEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace StorageService
+{
+    class CustomerEntity : TableEntity
+    {
+        public CustomerEntity(string lastName, string firstName)
+        {
+            this.PartitionKey = lastName;
+            this.RowKey = firstName;
+        }
+
+        public CustomerEntity() { }
+    }
+}
--- StorageService/Function1.cs
+++ StorageService/Function1.cs
@@ -1,32 +1,51 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Extensions.Configuration;
 
 namespace StorageService
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Create(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log, ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string email = req.Query["email"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            CloudStorageAccount storageAccount = new CloudStorageAccount(
+                new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(
+                    config["name"], config["key"]), true);
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Get a reference to a table named "peopleTable"
+            CloudTable peopleTable = tableClient.GetTableReference("peopleTable");
+
+            await peopleTable.CreateIfNotExistsAsync();
+
+            CustomerEntity customer1 = new CustomerEntity(email, email);
+
+            TableOperation insertOperation = TableOperation.Insert(customer1);
+            await peopleTable.ExecuteAsync(insertOperation);
+
+            return email != null
+                ? (ActionResult)new OkObjectResult($"Hello, {email}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
